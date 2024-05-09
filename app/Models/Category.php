22b0fc3c6947e86d6b61127fbe701f97776cd8e7
rizkijanuarr <bukanrizkijanuar@gmail.com>
2024-05-09 10:43:53 +0700

    feat: menambahkan accessor pada masing masing table yang ada image nya

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
@@ -19,5 +20,13 @@ class Category extends Model
     public function posts()
     {
         return $this->hasMany(Post::class);
+    }
+
+    // MENGGUNAKAN ACCESSOR
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => asset('/storage/categories/' . $image),
+        );
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -30,5 +31,13 @@ class Post extends Model
     public function views()
     {
         return $this->hasMany(PostView::class);
+    }
+
+    // MENGGUNAKAN ACCESSOR
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => asset('/storage/posts/' . $image),
+        );
     }
 }

7d37b8d4ef7794e28c7e534ca4a82afa488e6673
rizkijanuarr <bukanrizkijanuar@gmail.com>
2024-05-09 09:59:33 +0700

    :tada: Initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+
+    // AFTER INSTALL SPATIE PERMISSIONS
+    protected $guard_name ='api';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    // AFTER INSTALL SPATIE PERMISSIONS
+    public function getPermissionArray()
+    {
+        return $this->getAllPermissions()->mapWithKeys(function($pr){
+            return [$pr['name'] => true];
+        });
+
+    }
+
+    // AFTER INSTALL JWT AUTHENTICATION
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+}

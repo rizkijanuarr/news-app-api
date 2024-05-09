eae7dbc0f8b8be20d38c56dd07e404aa78417136
rizkijanuarr <bukanrizkijanuar@gmail.com>
2024-05-09 11:12:22 +0700

    feat: membuat restful api login

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// LOGIN
+Route::post('/login', [App\Http\Controllers\Api\Auth\LoginController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// MIDDLEWARE AUTH
+Route::group(['middleware' => 'auth:api'], function() {
+
+    //logout
+    Route::post('/logout', [App\Http\Controllers\Api\Auth\LoginController::class, 'logout']);
+
 });

f31416eddcb815a68fc1ba7902c9c13904f8b45d
rizkijanuarr <bukanrizkijanuar@gmail.com>
2024-05-09 10:11:47 +0700

    feat: menambahkan models, migration post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // MENYESUAIKAN KELAS
+    protected $fillable = [
+        'title', 'slug', 'category_id', 'user_id', 'content', 'image'
+    ];
 }
--- database/migrations/2024_05_09_030828_create_posts_table.php
+++ database/migrations/2024_05_09_030828_create_posts_table.php
@@ -6,15 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    // MENYESUAIKAN KELAS
+    public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->foreignId('category_id')->references('id')->on('categories')->cascadeOnDelete();
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->text('content');
+            $table->string('image');
             $table->timestamps();
         });
     }
